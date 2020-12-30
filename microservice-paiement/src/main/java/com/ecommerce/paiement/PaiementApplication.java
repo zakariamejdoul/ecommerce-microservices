c4dc8370ed52a034fa48daa808498be15a3ec303
Zakaria Mejdoul <47991093+zakariamejdoul@users.noreply.github.com>
2020-12-30 23:36:10 +0100

    init

---
+++ microservice-paiement/src/main/java/com/ecommerce/paiement/PaiementApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.paiement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaiementApplication.class, args);
+    }
+
+}
---
+++ microservice-paiement/src/test/java/com/ecommerce/paiement/PaiementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.paiement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
