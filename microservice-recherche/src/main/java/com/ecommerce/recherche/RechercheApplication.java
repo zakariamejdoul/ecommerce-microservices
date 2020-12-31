b7da4b1bc8feb3c8357a1cb0ecea73bec54aa2d8
Zakaria Mejdoul <47991093+zakariamejdoul@users.noreply.github.com>
2020-12-31 17:03:59 +0100

    init

---
+++ microservice-recherche/src/main/java/com/ecommerce/recherche/RechercheApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.recherche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechercheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RechercheApplication.class, args);
+    }
+
+}
---
+++ microservice-recherche/src/test/java/com/ecommerce/recherche/RechercheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.recherche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RechercheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
