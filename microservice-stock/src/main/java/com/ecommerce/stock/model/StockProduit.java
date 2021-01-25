dac6731477cada2cab7cbe6af15e0d01aa2ee6de
Naoual SMAILI <44554254+NaoualSmaili@users.noreply.github.com>
2021-01-25 21:57:17 +0100

    stockOk

---
+++ microservice-stock/src/main/java/com/ecommerce/stock/model/StockProduit.java
@@ -0,0 +1,35 @@
+package com.ecommerce.stock.model;
+
+public class StockProduit {
+    private int id;
+    private int quantite;
+
+    public StockProduit(){
+
+    }
+
+    public StockProduit(int id, int quantite){
+        this.id=id;
+        this.quantite=quantite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public Boolean estEpuise(int id) {
+        return this.id == id && this.quantite <= 0;
+    }
+}
