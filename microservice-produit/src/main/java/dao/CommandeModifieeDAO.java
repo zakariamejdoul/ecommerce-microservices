037d0269c67d8c3b800092a720e41124aef51f9c
youcef-ely <76822707+youcef-ely@users.noreply.github.com>
2021-02-01 12:13:56 +0100

    Delete microservice-produit directory

--- microservice-produit/src/main/java/dao/CommandeModifieeDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.CommandeModifiée;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommandeModifieeDAO extends JpaRepository<CommandeModifiée, Integer> {
-    Iterable<CommandeModifiée> findCommandeModifiéeByIdClient(int idClient);
-
-}
