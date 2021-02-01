2b0eaf310aa555c7c2715718ea161b5712a342ee
youcef-ely <76822707+youcef-ely@users.noreply.github.com>
2021-02-01 12:25:23 +0100

    Delete microservice-evalution directory

--- microservice-evalution/src/main/java/com/ecommerce/microserviceevalution/model/ResourceNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.ecommerce.microserviceevalution.model;
-
-public class ResourceNotFoundException extends Exception {
-}
--- microservice-evalution/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO avis VALUES(001, 'Tres bonne qualite' , 1, 12);
-INSERT INTO avis VALUES(002, 'Passable' , 2, 12);
-INSERT INTO avis VALUES(003, 'Veuillez reparer votre machine' , 2, 13);
