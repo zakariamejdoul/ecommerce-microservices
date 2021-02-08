44798e1459bf7239d4d3610d1ca505ec97b7d006
Zakaria Mejdoul <47991093+zakariamejdoul@users.noreply.github.com>
2021-02-08 19:19:28 +0100

    zuul api gateway

--- zuul-api-gateway/src/main/java/com/ecommerce/zuulapigateway/ZuulLoggingFilter.java
+++ zuul-api-gateway/src/main/java/com/ecommerce/zuulapigateway/ZuulLoggingFilter.java
@@ -6,7 +6,6 @@ import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Component
