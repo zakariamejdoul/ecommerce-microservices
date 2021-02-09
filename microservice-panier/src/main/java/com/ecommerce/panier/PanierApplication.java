3493aef2bb69801473af0d3ee1c493d41dcd7305
Aouissi Abdessamad <abdoawis34@gmail.com>
2021-02-09 23:30:06 +0000

    panierOk

--- microservice-panier/src/main/java/com/ecommerce/panier/PanierApplication.java
+++ microservice-panier/src/main/java/com/ecommerce/panier/PanierApplication.java
@@ -2,12 +2,16 @@ package com.ecommerce.panier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class PanierApplication {
 
     @Bean
