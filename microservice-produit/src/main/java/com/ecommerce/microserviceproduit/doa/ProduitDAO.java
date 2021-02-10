21190065c1d56cb00048835471d205698bc998d7
youcef-ely <76822707+youcef-ely@users.noreply.github.com>
2021-02-10 12:32:15 +0100

    Delete microservice-produit directory

--- microservice-produit/src/main/java/com/ecommerce/microserviceproduit/doa/ProduitDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.microserviceproduit.doa;
-
-import com.ecommerce.microserviceproduit.model.Produit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProduitDAO extends JpaRepository<Produit, Long> {
-}
